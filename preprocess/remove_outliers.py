a66f688d488979f6eeb6babafd6a9fff845aeab1
walkenzhong <walkenzhong@gmail.com>
2019-11-20 13:36:12 +1100

    update normalization
    Signed-off-by: walkenzhong <walkenzhong@gmail.com>

--- preprocess/remove_outliers.py
+++ preprocess/remove_outliers.py
@@ -12,7 +12,7 @@ def get_line(data):
 
 def normalization(all_data):
     #print(all_data.columns.values)
-    for i in range(2,len(all_data.columns.values)):
+    for i in range(1,len(all_data.columns.values)):
         #print(all_data.columns.values[i])
         all_data[all_data.columns.values[i]] = get_line(all_data[all_data.columns.values[i]])
     all_data.to_csv("data/all_features_normalised.csv")
