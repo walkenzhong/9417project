7e7141b46c9cc6c2def2406acf4fb2520c3644a3
walkenzhong <walkenzhong@gmail.com>
2019-11-21 17:32:55 +1100

    add train data for missing post
    Signed-off-by: walkenzhong <walkenzhong@gmail.com>

--- processed_output/low_and_high.py
+++ processed_output/low_and_high.py
@@ -9,28 +9,35 @@ def get_file(filename):
     return all_file
 
 def combine():
-    flourishing = get_file('Flourishing_result_without0.csv')
+    flourishing = get_file('..\\train_missing_post\\features_and_panas_values_pre_label_all_no_missing_post.csv')
     #panas= get_file('panas_result_without0.csv')
     #data = pd.merge(flourishing,panas)
     return flourishing
 
 def from_number_to_low_high():
     data = combine()
-    for i in range(1,len(data.columns.values)):
-        mean_num = np.mean(data[data.columns.values[i]])
-        std_num = np.std(data[data.columns.values[i]],ddof=1)
-        print(data.columns.values[i])
-        for j in range(len(data[data.columns.values[i]])):
-            now_num = (data[data.columns.values[i]][j] - mean_num)/std_num
-            if now_num > 1:
-                data[data.columns.values[i]][j] = 1
-            elif now_num < -1:
-                data[data.columns.values[i]][j] = -1
-            else:
-                data[data.columns.values[i]][j] = 0
+    print(data.columns.values[-2:-1])
+    mean_num = np.mean(data['post_positive_label'])
+    #print(mean_num)
+    #print(data['pre_label'][48])
+    std_num = np.std(data['post_positive_label'])
+    for i in range(len(data['post_positive_label'])):
+        now_num = (data['post_positive_label'][i] - mean_num)/std_num
+        if now_num < -1:
+            data['post_positive_label'][i] = 0
+        else:
+            data['post_positive_label'][i] = 1
+    mean_num = np.mean(data['post_negative_label'])
+    #print(mean_num)
+    #print(data['pre_label'][48])
+    std_num = np.std(data['post_negative_label'])
+    for i in range(len(data['post_negative_label'])):
+        now_num = (data['post_negative_label'][i] - mean_num)/std_num
+        if now_num < -1:
+            data['post_negative_label'][i] = 0
+        else:
+            data['post_negative_label'][i] = 1
     return data
-
-
 
 
 
@@ -38,4 +45,4 @@ def from_number_to_low_high():
 
 if __name__ == '__main__':
     data = from_number_to_low_high()
-    data.to_csv("Flourishing_result_without0_label.csv",encoding = 'gbk')
+    data.to_csv("features_and_panas_values_pre_label_all_no_missing_post_with_post_label.csv",encoding = 'gbk')
